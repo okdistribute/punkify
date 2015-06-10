10f2bc119d188c1bd6fa4178c385c11525067700
Karissa McKelvey <krmckelv@gmail.com>
2015-06-10 01:01:06 -0700

    only posers use whitespace

---
+++ cli.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+var punkify = require('./')
+var fs = require('fs')
+
+var file = process.argv[2]
+var out = process.argv[3]
+
+var reader = fs.createReadStream(file)
+var writer = fs.createWriteStream(out)
+
+writer.write('//{};{};THIS FILE IS FUCKING PUNK{};{};\n')
+
+reader.pipe(punkify()).pipe(writer)
